fba40b0311fb940b9c39a0d59fea585aa831d404
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-08-08 11:06:01 +0200

    Implemented CollecObservations() and AgentReset().
    Now implementing  AgentAction().

--- unity-environment/Assets/_Projects/CreatingMLAgentFromScratch/Platform/CreatingMLAgentFromScratchAgent.cs
+++ unity-environment/Assets/_Projects/CreatingMLAgentFromScratch/Platform/CreatingMLAgentFromScratchAgent.cs
@@ -7,12 +7,19 @@ using UnityEngine.Assertions;
 public class CreatingMLAgentFromScratchAgent : Agent
 {
     [SerializeField] private GameObject _ball = null;
+    private Rigidbody _ballRigidbody = null;
+    private Vector3 _ballStartPosition = Vector3.zero;
+
+    private void Start ()
+    {
+        _ballRigidbody = _ball.GetComponent<Rigidbody>();
+        Assert.IsNotNull(_ballRigidbody);
+
+        _ballStartPosition = _ball.transform.position;
+    }
 
     public override void CollectObservations ()
     {
-        var ballRigidbody = _ball.GetComponent<Rigidbody>();
-        Assert.IsNotNull(ballRigidbody);
-
         var state = new List<float>()
         {
             transform.rotation.x,
@@ -20,11 +27,61 @@ public class CreatingMLAgentFromScratchAgent : Agent
             _ball.transform.position.x - transform.position.x,
             _ball.transform.position.y - transform.position.y,
             _ball.transform.position.z - transform.position.z,
-            ballRigidbody.velocity.x,
-            ballRigidbody.velocity.y,
-            ballRigidbody.velocity.z,
+            _ballRigidbody.velocity.x,
+            _ballRigidbody.velocity.y,
+            _ballRigidbody.velocity.z,
         };
-
         AddVectorObs(state);
+    }
+
+    public override void AgentReset ()
+    {
+        transform.rotation = Quaternion.identity;
+        _ballRigidbody.velocity = Vector3.zero;
+        _ballRigidbody.MovePosition(_ballStartPosition);
+    }
+
+    /// <summary>
+    /// Controls actual movement of the platform.
+    /// Converts Q-values to actions.
+    /// </summary>
+    /// <param name="vectorAction"></param>
+    /// <param name="textAction"></param>
+    public override void AgentAction (float[] vectorAction, string textAction)
+    {
+        Assert.IsNotNull(brain);
+
+        // If using continuouse actions.
+        if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous)
+        {
+            // Use vectorAactions as continuous values.
+            transform.Rotate(Vector3.forward, vectorAction[0]);
+            transform.Rotate(Vector3.right, vectorAction[1]);
+
+            // Ball hasn't been dropped.
+            if (!IsDone())
+            {
+                SetReward(0.1f);
+            }
+        }
+        // If using discrete actions.
+        else
+        {
+            int action = (int)vectorAction[0];
+            float tiltSpeed = 2f;
+
+            // Z-rotation.
+            if (action == 0 || action == 1)
+            {
+                if (action == 0)
+                {
+                    transform.Rotate(Vector3.back, tiltSpeed);
+                }
+                else
+                {
+                    transform.Rotate(Vector3.forward, tiltSpeed);
+                }
+            }
+        }
     }
 }
