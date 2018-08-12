596f1066c8c5e138dcc0a52b55b34ee07fe82412
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-08-12 12:55:39 +0200

    Set up AvoidingMLAgentAgent.cs fields.

--- unity-environment/Assets/_Projects/AnAvoidingMLAgent/Agent/AvoidingMLAgentAgent.cs
+++ unity-environment/Assets/_Projects/AnAvoidingMLAgent/Agent/AvoidingMLAgentAgent.cs
@@ -6,8 +6,14 @@ using UnityEngine.Assertions;
 
 public class AvoidingMLAgentAgent : Agent
 {
-    [SerializeField] private GameObject _topBorder;
-    [SerializeField] private GameObject _bottomBorder;
-    [SerializeField] private GameObject _leftBorder;
-    [SerializeField] private GameObject _rightBorder;
+    [SerializeField] private GameObject _topBorder = null;
+    [SerializeField] private GameObject _bottomBorder = null;
+    [SerializeField] private GameObject _leftBorder = null;
+    [SerializeField] private GameObject _rightBorder = null;
+    private float _force = 10f;
+    private Vector2 _startPosition = Vector2.zero;
+    private Rigidbody2D _rigidBody2D = null;
+    private bool _hasCrashed = false;
+
+
 }
