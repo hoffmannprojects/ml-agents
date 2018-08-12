d581290c2b74570983c4f19015ba672154f75b7d
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-08-12 12:50:22 +0200

    Created AvoidingMLAgentAgent.cs

---
+++ unity-environment/Assets/_Projects/AnAvoidingMLAgent/Agent/AvoidingMLAgentAgent.cs
@@ -0,0 +1,13 @@
+﻿using MLAgents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class AvoidingMLAgentAgent : Agent
+{
+    [SerializeField] private GameObject _topBorder;
+    [SerializeField] private GameObject _bottomBorder;
+    [SerializeField] private GameObject _leftBorder;
+    [SerializeField] private GameObject _rightBorder;
+}
